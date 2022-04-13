f7ba79e9bc27a14532b192934ad36a7823818069
xodwkX2 <xodwkx2@gmail.com>
2022-04-13 15:08:15 +0900

    jade2pug

--- app.js
+++ app.js
@@ -8,20 +8,17 @@ var bodyParser = require('body-parser');
 var db = require('./model/db');
 var client = require('./model/clients');
 var branch = require('./model/branches');
-var nat   = require('./model/nats');
 
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var clientsRouter = require('./routes/nfv');
-var natRouter = require('./routes/nfvNAT');
-
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -29,11 +26,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
 app.use('/', indexRouter);
 
 app.use('/nfv', clientsRouter);
-app.use('/nfvNAT', natRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
